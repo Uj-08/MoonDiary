50a709a83ff9bfcf1b14b66ba8707bfe3789a40b
Ujjwal Pandey <ujjwalpandey24@gmail.com>
2022-04-29 22:36:12 +0530

    nav features added

--- New/scripts/script.js
+++ New/scripts/script.js
@@ -1,9 +1,25 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0];
-const navbarLinks = [document.getElementsByClassName('nav__links')[0], document.getElementsByClassName('nav__social')[0]]
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = [
+  document.getElementsByClassName("nav__links")[0],
+  document.getElementsByClassName("nav__social")[0],
+];
 
-toggleButton.addEventListener('click', () => {
-  navbarLinks.forEach(element => {
-    element.classList.toggle('active')
+toggleButton.addEventListener("click", () => {
+  navbarLinks.forEach((element) => {
+    element.classList.toggle("active");
     // console.log(event)
   });
+});
+
+const navbar = document.getElementById("navbar");
+let lastScroll = window.scrollY;
+window.addEventListener("scroll", () => {
+  if (lastScroll < window.scrollY) {
+    console.log("We are going down");
+    navbar.classList.add("hidden");
+  } else {
+    console.log("We are going up");
+    navbar.classList.remove("hidden");
+  }
+  lastScroll = window.scrollY;
 });
