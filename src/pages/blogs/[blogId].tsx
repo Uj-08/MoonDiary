a1def07318f09f8c7875c8290db0a44d0c0ac5c9
Ujjwa Pandey <ujjwal.pandey@lenskart.in>
2023-04-24 10:49:03 +0530

    loading blog

--- src/pages/blogs/[blogId].tsx
+++ src/pages/blogs/[blogId].tsx
@@ -3,9 +3,12 @@ import BlogTitleComponent from "@/components/Blog/BlogTitle/BlogTitle.component"
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import BlogComponent from "@/components/Blog/Blog.component";
+import { Loading } from "..";
+import Image from "next/image";
 
 export default function Blog() {
     const [blogData, setBlogData] = useState<{blogTitle: string, blogImg: string, blogData: string}>();
+    const [loading, setLoading] = useState(true);
     const router = useRouter()
     useEffect(() => {
         if(router.isReady){
@@ -13,15 +16,25 @@ export default function Blog() {
             fetch(`/api/blogs/${blogId}`)
             .then(data => data.json())
             .then(data => setBlogData(data.blog))
+            .then(() => setLoading(false));
         }
     }, [router.isReady, router.query.blogId])
     console.log(blogData)
     return (
-        <Base>
-            <BlogTitleComponent>
-                {blogData?.blogTitle}
-            </BlogTitleComponent>
-            <BlogComponent blogImg={blogData?.blogImg} blogData={blogData?.blogData}/>
-        </Base>
+        <>
+            { loading &&
+                <Loading>
+                    <div>
+                    <Image src="/logo.png" alt="loading" fill={true}/>
+                    </div>
+                </Loading>
+            }
+            <Base>
+                <BlogTitleComponent>
+                    {blogData?.blogTitle}
+                </BlogTitleComponent>
+                <BlogComponent blogImg={blogData?.blogImg} blogData={blogData?.blogData}/>
+            </Base>
+        </>
     );
 }
