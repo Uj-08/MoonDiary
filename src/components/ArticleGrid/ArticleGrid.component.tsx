600bce42bf233dbc9d070d9a3cd1acc70ac73ff6
Ujjwal Pandey <ujjwal.pandey@lenskart.in>
2025-06-01 18:45:06 +0530

    fix: empty check

--- src/components/ArticleGrid/ArticleGrid.component.tsx
+++ src/components/ArticleGrid/ArticleGrid.component.tsx
@@ -1,41 +1,38 @@
 import { useEffect, useState } from "react";
- import { Container, Title, Grid } from "./ArticleGrid.styles";
- import PostCard from "../ArticleCard/PostCard.component";
- import DynamicCard from "../ArticleCard/DynamicCard.component";
+import { Container, Title, Grid } from "./ArticleGrid.styles";
+import PostCard from "../ArticleCard/PostCard.component";
+import DynamicCard from "../ArticleCard/DynamicCard.component";
 import { getCookie, hasCookie } from "cookies-next";
 import jwtDecode from "jwt-decode";
- 
- export default function ArticleGrid ({ blogs }: { blogs: any}) {
 
-    const [client, setClient] = useState<{email: string}>();
-    
+export default function ArticleGrid({ blogs }: { blogs: any }) {
+  const [client, setClient] = useState<{ email: string }>();
 
-    useEffect(() => {
-        if(hasCookie("clientMD")) {
-            const cookie = getCookie("clientMD");
-            if(typeof(cookie) === "string"){
-                setClient(jwtDecode(cookie))
-            }
-        }
-    }, [])
-    
-    return (
-        <Container>
-            {/* <Title>
+  useEffect(() => {
+    if (hasCookie("clientMD")) {
+      const cookie = getCookie("clientMD");
+      if (typeof cookie === "string") {
+        setClient(jwtDecode(cookie));
+      }
+    }
+  }, []);
+
+  return (
+    <Container>
+      {/* <Title>
                 Recent:
             </Title> */}
-            <Grid>
-                {/* <Card/> */}
-                {blogs.map((blog: any, idx: number) => {
-                    return (
-                        <DynamicCard key={idx} blog={blog} clientEmail={client?.email} />
-                    )
-                })}
-                {
-                    client && (client?.email === "ujjwalpandey24@gmail.com" || client?.email === "sinhashairee6@gmail.com") &&
-                    <PostCard />
-                }
-            </Grid>
-        </Container>
-    );
- }
+      <Grid>
+        {/* <Card/> */}
+        {blogs?.map((blog: any, idx: number) => {
+          return (
+            <DynamicCard key={idx} blog={blog} clientEmail={client?.email} />
+          );
+        })}
+        {client &&
+          (client?.email === "ujjwalpandey24@gmail.com" ||
+            client?.email === "sinhashairee6@gmail.com") && <PostCard />}
+      </Grid>
+    </Container>
+  );
+}
