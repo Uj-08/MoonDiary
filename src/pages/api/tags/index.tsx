78da343dfc72e6049144edecf5b87a3d06ecdf8c
Ujjwal Pandey <ujjwal.pandey@lenskart.in>
2025-06-05 11:29:29 +0530

    refactor: comment unused

--- src/pages/api/tags/index.tsx
+++ src/pages/api/tags/index.tsx
@@ -20,42 +20,42 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
             console.error("Tag search error:", err);
             res.status(500).json({ error: "Internal server error" });
         }
-    } else if (req.method === "POST") {
-        const body = req.body;
-        let newTag = new TagsModel({
-            name: body.blogTitle,
-            blogIds: [body.blogId],
-        });
-        try {
-            const savedObj = await newTag.save();
-            const id = savedObj._id;
-            res.status(201).json({ id })
-        } catch (err) {
-            console.log(err);
-            res.status(500).json({ message: "Could not connect to database." })
-        }
-    } else if (req.method === "PUT") {
-        try {
-            const body = req.body;
-            const blogId = body.blogId;
-            const tagId = body.tagId;
+        // } else if (req.method === "POST") {
+        //     const body = req.body;
+        //     let newTag = new TagsModel({
+        //         name: body.blogTitle,
+        //         blogIds: [body.blogId],
+        //     });
+        //     try {
+        //         const savedObj = await newTag.save();
+        //         const id = savedObj._id;
+        //         res.status(201).json({ id })
+        //     } catch (err) {
+        //         console.log(err);
+        //         res.status(500).json({ message: "Could not connect to database." })
+        //     }
+        // } else if (req.method === "PUT") {
+        //     try {
+        //         const body = req.body;
+        //         const blogId = body.blogId;
+        //         const tagId = body.tagId;
 
-            const tag = await TagsModel.findById(tagId);
-            await TagsModel.findByIdAndUpdate(
-                tagId,
-                {
-                    $set: {
-                        blogIds: [...tag.blogIds, blogId]
-                    }
-                }
-            );
+        //         const tag = await TagsModel.findById(tagId);
+        //         await TagsModel.findByIdAndUpdate(
+        //             tagId,
+        //             {
+        //                 $set: {
+        //                     blogIds: [...tag.blogIds, blogId]
+        //                 }
+        //             }
+        //         );
 
-            res.status(200).json({ tagId });
-        } catch (err) {
-            console.log("error: ", err);
-            res.status(500).json({ error: err });
-        }
+        //         res.status(200).json({ tagId });
+        //     } catch (err) {
+        //         console.log("error: ", err);
+        //         res.status(500).json({ error: err });
+        //     }
+        // }
     }
 }
-
 export default connectDB(handler);
