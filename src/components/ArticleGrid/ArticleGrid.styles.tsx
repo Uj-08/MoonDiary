a5817031159afe5d841129f7c36f819d9cf83e57
Ujjwal Pandey <ujjwal.pandey@lenskart.in>
2025-06-09 19:18:55 +0530

    fix: css

--- src/components/ArticleGrid/ArticleGrid.styles.tsx
+++ src/components/ArticleGrid/ArticleGrid.styles.tsx
@@ -1,42 +1,42 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const Container = styled.section`
-    padding: 1rem 8rem;
-    @media (max-width: 1200px) {
-        padding: 0 4rem;
-    }
-    @media (max-width: 812px) {
-        padding: 0 2rem;
-    }
-    @media (max-width: 450px) {
-        padding: 0 1rem;
-    }
+  padding: 1rem 8rem;
+  @media (max-width: 1200px) {
+    padding: 0 4rem;
+  }
+  @media (max-width: 812px) {
+    padding: 0 2rem;
+  }
+  @media (max-width: 450px) {
+    padding: 0 1rem;
+  }
 `;
 
 export const Title = styled.h2`
-    font-size: 25px;
-    font-family: "Arimo", sans-serif;
-    font-weight: 900;
-    /* letter-spacing: 1.01px; */
-    text-transform: uppercase;
+  font-size: 25px;
+  font-family: "Arimo", sans-serif;
+  font-weight: 900;
+  /* letter-spacing: 1.01px; */
+  text-transform: uppercase;
 `;
 
 export const Grid = styled.div`
-    display: grid;
-    padding-top: 1rem;
+  display: grid;
+  padding-top: 1rem;
 
-    & > :first-child {
-        grid-column: 1 / -1;
-    }
+  & > :first-child {
+    grid-column: 1 / -1;
+  }
 
-    grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
-    gap: 1rem 5px;
-    @media (max-width: 768px) {
-        grid-template-columns: repeat(auto-fill, minmax(330px, 1fr));
-    }
-    @media (max-width: 380px) {
-        grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
-    }
+  grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
+  gap: 1rem 5px;
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(auto-fill, minmax(330px, 1fr));
+  }
+  @media (max-width: 380px) {
+    grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+  }
 `;
 
 export const SortContainer = styled.div`
@@ -51,11 +51,11 @@ export const SortContainer = styled.div`
     align-items: center;
   }
   @media (max-width: 728px) {
-        justify-content: center;
-    }
-   @media (max-width: 500px) {
-        font-size: 14px;
-    }
+    justify-content: center;
+  }
+  @media (max-width: 500px) {
+    font-size: 12px;
+  }
 `;
 
 export const Label = styled.label`
@@ -74,6 +74,9 @@ export const Select = styled.select`
   cursor: pointer;
   transition: border 0.2s ease;
   box-shadow: 0 2px 6px rgba(189, 16, 224, 0.08);
+  @media (max-width: 500px) {
+    font-size: 12px;
+  }
 
   &:focus {
     border-color: rgba(189, 16, 224, 0.5);
