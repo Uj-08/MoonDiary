c1c54ef39a93b714d9db088277c6f8e64ce6fbf7
Ujjwal Pandey <ujjwalpandey24@gmail.com>
2025-06-13 21:22:32 +0530

    refactor

--- src/components/Editor/BlogContentStyle.ts
+++ src/components/Editor/BlogContentStyle.ts
@@ -35,26 +35,44 @@ export const blogRenderStyle = css`
   font-size: 16px;
   line-height: 1.6;
 
-  h1, h2, h3, h4, h5, h6 {
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
     font-weight: 600;
     line-height: 1.3;
     /* margin: 1.5em 0 1em; */
     margin-bottom: 1em;
   }
 
-  h1 { font-size: 2rem; }
-  h2 { font-size: 1.75rem; }
-  h3 { font-size: 1.5rem; }
-  h4 { font-size: 1.25rem; }
-  h5 { font-size: 1.1rem; }
-  h6 { font-size: 1rem; }
+  h1 {
+    font-size: 2rem;
+  }
+  h2 {
+    font-size: 1.75rem;
+  }
+  h3 {
+    font-size: 1.5rem;
+  }
+  h4 {
+    font-size: 1.25rem;
+  }
+  h5 {
+    font-size: 1.1rem;
+  }
+  h6 {
+    font-size: 1rem;
+  }
 
   p {
     margin-bottom: 0.1em;
     word-break: break-word;
   }
 
-  ul, ol {
+  ul,
+  ol {
     padding-left: 2em;
     margin-bottom: 0.5em;
   }
