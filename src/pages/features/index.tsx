1cd8aedabc0a7070e068733ff45b41f6e1d92a95
Ujjwal Pandey <ujjwalpandey24@gmail.com>
2025-06-25 17:59:36 +0530

    refactor: tags

--- src/pages/features/index.tsx
+++ src/pages/features/index.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import Head from "next/head";
 import { GetServerSideProps } from "next";
-import { hasCookie } from "cookies-next";
-import { COOKIE_NAME } from "@/helpers/constants";
 import { FeaturesTagType } from "@/types/tag";
 import FeaturesComponent from "@/components/Pages/Features/Features.component";
 
@@ -26,21 +24,9 @@ const Features = ({ tags }: { tags: FeaturesTagType[] }) => {
 export default Features;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-	const { req, res } = context;
-
 	const API_INSTANCE = new URL("/api/tags", process.env.NEXT_PUBLIC_BASE_URL);
-
-	const isSessionAvailable = hasCookie(COOKIE_NAME, { req, res });
-	if (isSessionAvailable) {
-	}
 	try {
-		const apiRes = await fetch(API_INSTANCE, {
-			method: "GET",
-			headers: {
-				"Content-Type": "application/json",
-				//   'x-session-token': getCookie(COOKIE_NAME, context) as string,
-			},
-		});
+		const apiRes = await fetch(API_INSTANCE);
 
 		if (!apiRes.ok) throw new Error(`API error: ${apiRes.status}`);
 
